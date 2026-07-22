1a3c161a98a93a3bb3280e76e668d8283bc8b8ab
Vaibhav Pujari <276vaibhav+github@gmail.com>
2026-07-22 19:04:29 +0530

    Exercise 1.29

---
+++ 1_29_simpsons-rule.rkt
@@ -0,0 +1,32 @@
+#lang sicp
+
+(define (sum term next a b)
+    (if (> a b) 
+        0
+        (
+            +
+            (term a)
+            (sum term next (next a) b)
+        )
+    )
+)
+
+(define (integral f a b n)
+    (define h (/ (- a b) n))
+    (define (term k)
+        (define (find-y) (f (+ a (* k h))))
+        (
+            *
+            (cond 
+                ((or (= k 0) (= k n)) 1)
+                ((even? k) 2)
+                (else 4)
+            )
+            (find-y)
+        )
+    )
+    (define (inc x) (+ x 1))
+    (/ (* h (sum term inc 0 n)) 3)
+)
+
+(define (cube x) (* x x x))

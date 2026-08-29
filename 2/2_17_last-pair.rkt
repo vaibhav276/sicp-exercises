da8e42f2fc0c1f886d64b5f136146a6023feb92c
Vaibhav Pujari <276vaibhav+github@gmail.com>
2026-08-29 09:08:35 +0530

    Exercise 2.17

---
+++ 2/2_17_last-pair.rkt
@@ -0,0 +1,25 @@
+; Exercise 2.17: Define a procedure last-pair that returns
+; the list that contains only the last element of a given (nonempty)
+; list:
+; (last-pair (list 23 72 149 34))
+; (34)
+
+#lang sicp
+
+(define (length l)
+  (define (iter res m)
+    (if (null? m)
+        res
+        (iter (+ 1 res) (cdr m))
+    )
+  )
+  (iter 0 l)
+)
+
+(define (last-pair l)
+  (if (= 1 (length l))
+      l
+      (last-pair (cdr l))
+  )
+)
+
